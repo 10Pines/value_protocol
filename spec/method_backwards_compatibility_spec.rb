d12a352d9abed0f1ea825d707ecdf16f70833a0b
Nicolas Papagna Maldonado <nicolas.papagna@8thlight.com>
2014-03-05 23:16:40 -0300

    Added specs to test backwards compatibility of Method and Proc when arguments are expected

--- spec/method_backwards_compatibility_spec.rb
+++ spec/method_backwards_compatibility_spec.rb
@@ -2,12 +2,20 @@ require 'spec_helper'
 
 describe 'Method backwards compatibility' do
 
-  it 'should not override :call behavior in Method' do
+  it 'should not override :call behavior in Method when arguments are not expected' do
     upcase_method = 'ruby'.method :upcase
 
     result = upcase_method.call
 
     result.should == 'RUBY'
+  end
+
+  it 'should not override :call behavior in Method when arguments are expected' do
+    add_method = 1.method :+
+
+    result = add_method.call 1
+
+    result.should == 2
   end
 
   it 'should not override :to_proc behavior in Method' do
--- spec/proc_backwards_compatibility_spec.rb
+++ spec/proc_backwards_compatibility_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'Proc backwards compatibility' do
 
-  it 'should not override :call behavior in procs' do
+  it 'should not override :call behavior in procs when arguments are not expected' do
     proc = Proc.new { 7 }
 
     result = proc.call
@@ -10,12 +10,28 @@ describe 'Proc backwards compatibility' do
     result.should == 7
   end
 
-  it 'should not override :call behavior in lambdas' do
+  it 'should not override :call behavior in procs when arguments are expected' do
+    proc = Proc.new { |number| 1 + number }
+
+    result = proc.call 1
+
+    result.should == 2
+  end
+
+  it 'should not override :call behavior in lambdas when arguments are not expected' do
     lambda = lambda { 7 }
 
     result = lambda.call
 
     result.should == 7
+  end
+
+  it 'should not override :call behavior in lambdas when arguments are expected' do
+    lambda = lambda { |number| 1 + number }
+
+    result = lambda.call 1
+
+    result.should == 2
   end
 
   it 'should not override :to_proc behavior in procs' do
